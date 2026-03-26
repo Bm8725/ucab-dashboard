0c2b17ad81e4aaafa826c682d63a88896e48f992
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-26 18:25:37 +0200

    ucab app menu dash

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Declarăm o variabilă globală pentru a păstra instanța
+const globalForSupabase = global as unknown as { supabase: ReturnType<typeof createClient> };
+
+// Exportăm instanța existentă sau creăm una nouă dacă nu există
+export const supabase = globalForSupabase.supabase || createClient(supabaseUrl, supabaseAnonKey);
+
+// În modul de dezvoltare, salvăm instanța în obiectul global pentru a preveni duplicarea la Hot Reload
+if (process.env.NODE_ENV !== 'production') globalForSupabase.supabase = supabase;
